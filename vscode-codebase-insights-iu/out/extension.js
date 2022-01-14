005d67b760e84f217315eab2fccc5b6b53c99884
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2022-01-14 12:29:32 -0500

    Demo

--- vscode-codebase-insights-iu/out/extension.js
+++ vscode-codebase-insights-iu/out/extension.js
@@ -1,27 +1,157 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+exports.activate = void 0;
 const vscode = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const cats = {
+    'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+    'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
+    'Testing Cat': 'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif'
+};
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-codebase-insights-iu" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vscode-codebase-insights-iu.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from codebase-insights!');
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('vscode-codebase-insights-iu.start', () => {
+        CodingPanel.createOrShow(context.extensionUri);
+    }));
+    if (vscode.window.registerWebviewPanelSerializer) {
+        // Make sure we register a serializer in activation event
+        vscode.window.registerWebviewPanelSerializer(CodingPanel.viewType, {
+            async deserializeWebviewPanel(webviewPanel, state) {
+                console.log(`Got state: ${state}`);
+                // Reset the webview options so we use latest uri for `localResourceRoots`.
+                webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
+                CodingPanel.revive(webviewPanel, context.extensionUri);
+            }
+        });
+    }
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
-function deactivate() { }
-exports.deactivate = deactivate;
+function getWebviewOptions(extensionUri) {
+    return {
+        // Enable javascript in the webview
+        enableScripts: true,
+        // And restrict the webview to only loading content from our extension's `media` directory.
+        localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+    };
+}
+class CodingPanel {
+    constructor(panel, extensionUri) {
+        this._disposables = [];
+        this._panel = panel;
+        this._extensionUri = extensionUri;
+        // Set the webview's initial html content
+        this._update();
+        // Listen for when the panel is disposed
+        // This happens when the user closes the panel or when the panel is closed programmatically
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+        // Update the content based on view changes
+        this._panel.onDidChangeViewState(e => {
+            if (this._panel.visible) {
+                this._update();
+            }
+        }, null, this._disposables);
+        // Handle messages from the webview
+        this._panel.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'alert':
+                    vscode.window.showErrorMessage(message.text);
+                    return;
+            }
+        }, null, this._disposables);
+    }
+    static createOrShow(extensionUri) {
+        const column = vscode.window.activeTextEditor
+            ? vscode.window.activeTextEditor.viewColumn
+            : undefined;
+        // If we already have a panel, show it.
+        if (CodingPanel.currentPanel) {
+            CodingPanel.currentPanel._panel.reveal(column);
+            return;
+        }
+        // Otherwise, create a new panel.
+        const panel = vscode.window.createWebviewPanel(CodingPanel.viewType, 'Codebase Insights', column || vscode.ViewColumn.One, getWebviewOptions(extensionUri));
+        CodingPanel.currentPanel = new CodingPanel(panel, extensionUri);
+    }
+    static revive(panel, extensionUri) {
+        CodingPanel.currentPanel = new CodingPanel(panel, extensionUri);
+    }
+    doRefactor() {
+        // Send a message to the webview webview.
+        // You can send any JSON serializable data.
+        this._panel.webview.postMessage({ command: 'refactor' });
+    }
+    dispose() {
+        CodingPanel.currentPanel = undefined;
+        // Clean up our resources
+        this._panel.dispose();
+        while (this._disposables.length) {
+            const x = this._disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
+    _update() {
+        const webview = this._panel.webview;
+        // // Vary the webview's content based on where it is located in the editor.
+        // switch (this._panel.viewColumn) {
+        // 	case vscode.ViewColumn.Two:
+        // 		this._updateForCat(webview, 'Compiling Cat');
+        // 		return;
+        // 	case vscode.ViewColumn.Three:
+        // 		this._updateForCat(webview, 'Testing Cat');
+        // 		return;
+        // 	case vscode.ViewColumn.One:
+        // 	default:
+        this._updateForCat(webview, 'Coding Cat');
+        return;
+        // }
+    }
+    _updateForCat(webview, catName) {
+        this._panel.title = "Codebase Insights";
+        this._panel.webview.html = this._getHtmlForWebview(webview, cats[catName]);
+    }
+    _getHtmlForWebview(webview, catGifPath) {
+        // Local path to main script run in the webview
+        const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js');
+        // And the uri we use to load this script in the webview
+        const scriptUri = (scriptPathOnDisk).with({ 'scheme': 'vscode-resource' });
+        // Local path to css styles
+        const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
+        const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css');
+        const heatMapPath = vscode.Uri.joinPath(this._extensionUri, 'resources', 'Heatmap.png');
+        // Uri to load styles into webview
+        const stylesResetUri = webview.asWebviewUri(styleResetPath);
+        const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+        const heatMapUri = webview.asWebviewUri(heatMapPath);
+        // Use a nonce to only allow specific scripts to be run
+        const nonce = getNonce();
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${stylesResetUri}" rel="stylesheet">
+				<link href="${stylesMainUri}" rel="stylesheet">
+				<title>Codebase Insights</title>
+			</head>
+			<body>
+				<h1 style="background-color: white;">Welcome To Codebase Insights</h1>
+				<img src="https://i.imgur.com/QI28Ewm.png" width=300 height=300></img>
+				<p>This is all HTML - in theory we can handle complex UIs as well</p>
+			</html>`;
+    }
+}
+CodingPanel.viewType = 'vscode-codebase-insights-iu';
+function getNonce() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
 //# sourceMappingURL=extension.js.map
