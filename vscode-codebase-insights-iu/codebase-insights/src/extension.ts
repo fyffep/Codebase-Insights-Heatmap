31df29ba76d8bcc36bb02c8fadebb88f03ec9752
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2022-01-19 15:49:01 -0500

    TreeViewContainer working

--- vscode-codebase-insights-iu/codebase-insights/src/extension.ts
+++ vscode-codebase-insights-iu/codebase-insights/src/extension.ts
@@ -12,22 +12,6 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   vscode.window.showInformationMessage('Hello World from Codebase Insights!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  //   let disposable = vscode.commands.registerCommand(
-  //     'codebase-insights.helloWorld',
-  //     () => {
-  //       // The code you place here will be executed every time your command is executed
-  //       // Display a message box to the user
-  //       vscode.window.showInformationMessage(
-  //         'Hello World from Codebase Insights!'
-  //       );
-  //     }
-  //   );
-
-  //   context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
