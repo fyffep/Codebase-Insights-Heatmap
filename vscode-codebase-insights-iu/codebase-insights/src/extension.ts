8624ba40ee915b8053a36613945ab36245447c9a
isaiahsherfick <isaiahsherfick8@gmail.com>
2022-01-20 10:24:25 -0500

    Trying to get the different views to appear in our treeview

--- vscode-codebase-insights-iu/codebase-insights/src/extension.ts
+++ vscode-codebase-insights-iu/codebase-insights/src/extension.ts
@@ -11,7 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "codebase-insights" is now active!'
   );
 
-  vscode.window.showInformationMessage('Hello World from Codebase Insights!');
+  vscode.window.showInformationMessage('fdasfsfafreqw');
+  //vscode.window.registerTreeDataProvider('codebaseInsightsTree', new CodebaseInsightsViewProvider());
 }
 
 export class CodeBaseInsightsView extends vscode.TreeItem {
@@ -21,7 +22,7 @@ export class CodeBaseInsightsView extends vscode.TreeItem {
     public readonly description: string
   ) {
     super(label, collapsibleState);
-    this.tooltip = `${this.label}`;
+    this.tooltip = this.label;
     this.description = this.description;
   }
 }
@@ -30,10 +31,22 @@ export class CodebaseInsightsViewProvider implements vscode.TreeDataProvider<Cod
   getTreeItem(element: CodeBaseInsightsView) {
     return element;
   }
+
   getChildren(element?: CodeBaseInsightsView): Thenable<CodeBaseInsightsView[]> {
-    return Promise.resolve([]);
+    return Promise.resolve(this.getViews());
+  }
+
+  private getViews(): CodeBaseInsightsView[] {
+    let views = [];
+    let overview = new CodeBaseInsightsView("Overview", vscode.TreeItemCollapsibleState.None, "A broad overview of your codebase to get a quick peek at your project");
+    let map = new CodeBaseInsightsView("Code Map", vscode.TreeItemCollapsibleState.None,"A configurable heatmap for viewing your codebase graphically and analyzing various metrics as a gradient");
+    let knowledge = new CodeBaseInsightsView("Knowledge Graph", vscode.TreeItemCollapsibleState.None, "A graph showing the areas where each contributor on your project has knowledge");
+    let insights = new CodeBaseInsightsView("Insights", vscode.TreeItemCollapsibleState.None,"Create custom queries to analyze your codebase");
+    views = [overview, map, knowledge, insights];
+    return views;
   }
 }
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
