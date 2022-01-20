28cbccf5a061b36a4f8c4bb08e678cda6270fad7
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2022-01-20 13:33:54 -0500

    Moved webviews from consts to factory methods

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+//Factory methods for our webviews using the singleton pattern
+function overviewPanel(): void {
+  const panel = vscode.window.createWebviewPanel(
+    'overviewPage',
+    'Overview',
+    vscode.ViewColumn.One,
+    {}
+  );
+  panel.webview.html = ""; //This is where we will put the html content for the view later
+}
+
+function codeMapPanel(): void {
+  const panel = vscode.window.createWebviewPanel(
+    'codeMapPage',
+    'Code Map',
+    vscode.ViewColumn.One,
+    {}
+  );
+  panel.webview.html = ""; //html for webview goes here
+}
+
+function knowledgeGraphPanel(): void {
+  const panel = vscode.window.createWebviewPanel(
+    'knowledgeGraph',
+    'Knowledge Graph',
+    vscode.ViewColumn.One,
+    {}
+  );
+  panel.webview.html = "";
+}
+
+function insightsPanel(): void {
+  const panel = vscode.window.createWebviewPanel(
+    'insights',
+    'Insights',
+    vscode.ViewColumn.One,
+    {}
+  );
+  panel.webview.html = "";
+}
+//Called when an event in package.json.activationEvents occurs
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Codebase Insights extension activated!');
 
   vscode.window.registerTreeDataProvider(
@@ -14,59 +49,32 @@ export function activate(context: vscode.ExtensionContext) {
     new CodebaseInsightsViewProvider()
   );
 
-  const overviewPanel = vscode.window.createWebviewPanel(
-    'overviewPage',
-    'Overview',
-    vscode.ViewColumn.One,
-    {}
-  );
-
-  const codemapPanel = vscode.window.createWebviewPanel(
-    'codeMapPage',
-    'Code Map',
-    vscode.ViewColumn.One,
-    {}
-  );
-
-  const knowledgeGraphPanel = vscode.window.createWebviewPanel(
-    'knowledgeGraph',
-    'Knowledge Graph',
-    vscode.ViewColumn.One,
-    {}
-  );
-
-  const insightsPanel = vscode.window.createWebviewPanel(
-    'insights',
-    'Insights',
-    vscode.ViewColumn.One,
-    {}
-  );
 
   context.subscriptions.push(
     vscode.commands.registerCommand('codebase-insights.overview', () => {
       // Create and show a new webview
-      overviewPanel;
+      overviewPanel();
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand('codebase-insights.code-map', () => {
       // Create and show a new webview
-      codemapPanel;
+      codeMapPanel();
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand('codebase-insights.knowledge-graph', () => {
       // Create and show a new webview
-      knowledgeGraphPanel;
+      knowledgeGraphPanel();
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand('codebase-insights.insights', () => {
       // Create and show a new webview
-      insightsPanel;
+      insightsPanel();
     })
   );
 }
