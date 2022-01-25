ef3416b3fa4e58122c76a58a7ba806130460bc1c
fyffep <pgf01@rocketmail.com>
2022-01-24 20:16:47 -0500

    Successfully sent a GET request

--- src/webviews/overview/overview.ts
+++ src/webviews/overview/overview.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as api from '../../api';
+import axios, { AxiosPromise } from 'axios';
 
 export function overviewHTML(cssUri:vscode.Uri): string {
 
@@ -8,6 +9,11 @@ export function overviewHTML(cssUri:vscode.Uri): string {
     let inactiveDevs: number = api.getNumberOfInactiveDevelopers();
     let commits: number = api.getNumberOfTotalCommits();
     let healthScore: string = api.getOverallCodebaseHealthScore();
+
+    //Hello World GET
+    helloWorldGetRequest().then(responseData => {
+        console.log(responseData); 
+    });
 
     return `
     <!DOCTYPE HTML>
@@ -22,8 +28,40 @@ export function overviewHTML(cssUri:vscode.Uri): string {
                 <h2> ${inactiveDevs} inactive developers out of ${developers} total developers</h2>
                 <h2> ${sloc} lines of code across ${commits} commits </h2>
                 <h2> Overall health score: ${healthScore} </h2>
+
+                <button onclick="myFunction2()">This is my broken button</button>
             </div>
         </body>
+
+        <script>
+            function myFunction2() {
+                console.log("Hello world");
+            }
+
+        </script>
     </HTML>
     `;
+}
+
+function helloWorldGetRequest(): AxiosPromise<any> 
+{
+    //Setup (TODO should be moved so that it is only done once)
+    const instance = axios.create({
+        baseURL: 'https://supercontinent-lutil.herokuapp.com/api',
+        timeout: 5000,
+        //headers: {'X-Custom-Header': 'foobar'}
+    });
+
+    //Send a request
+    var userId = '612854ee6aa27d439ed826e2';
+    return instance.get(`/account/all`)
+        .then((response) => {
+            //Return data from the axios promise
+            return response.data;
+        })
+        .catch(err => {
+            //Handle timeout or error
+            console.error(err);
+            return err;
+        });
 }
