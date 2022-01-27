78f369ce8ff7e3373dac2fd1c93297d499cf83ff
isaiahsherfick <isaiahsherfick8@gmail.com>
2022-01-27 10:14:19 -0500

    Got d3 imported to codemap view successfully

--- src/webviews/codeMap/codeMap.ts
+++ src/webviews/codeMap/codeMap.ts
@@ -11,11 +11,12 @@ export function codemapHTML(cssUri: vscode.Uri, scriptUri: vscode.Uri): string {
         <head>
             <meta charset="UTF-8" lang="en"/>
             <link rel="stylesheet" type="text/css" href="${cssUri}"/>
+            <script src="https://d3js.org/d3.v7.min.js"></script>
         </head>
         <body>
             <p> This text should be green </p>
         </body>
-        <script src="${scriptUri}"/>
+        <script src="${scriptUri}"></script>
     </HTML>
     `;
 }
--- src/webviews/codeMap/codeMapScript.js
+++ src/webviews/codeMap/codeMapScript.js
@@ -1,2 +1,1 @@
-var d3 = require('d3');
 d3.select("p").style("color","green");
