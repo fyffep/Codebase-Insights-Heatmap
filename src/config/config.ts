321096b37392134bbd4687e937284cbfb67e0c1c
isaiahsherfick <isaiahsherfick8@gmail.com>
2022-02-01 14:24:58 -0500

    Remove console.logs

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,10 +3,6 @@ import * as vscode from "vscode";
 export function getGitUrl(): string | undefined {
   let configuration = vscode.workspace.getConfiguration("codebase-insights");
   let repoUrl = configuration["repoURL"];
-  console.log("Get result:");
-  console.log(repoUrl);
-  console.log("Configuration from get function():");
-  console.log(configuration);
   if (repoUrl) {
     return repoUrl;
   } else {
@@ -16,9 +12,5 @@ export function getGitUrl(): string | undefined {
 
 export async function setGitUrl(url: string): Promise<void> {
   let configuration = vscode.workspace.getConfiguration("codebase-insights");
-  console.log("Before update:");
-  console.log(configuration);
   await configuration.update("repoURL", url, true);
-  console.log("After update:");
-  console.log(configuration);
 }
