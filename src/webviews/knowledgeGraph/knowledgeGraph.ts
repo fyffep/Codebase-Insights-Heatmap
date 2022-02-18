85d5f73c8d3c35beccd13622124e38cff47aeb5c
isaiahsherfick <isaiahsherfick8@gmail.com>
2022-02-18 11:19:11 -0500

    Added d3 to knowledge graph

--- src/webviews/knowledgeGraph/knowledgeGraph.ts
+++ src/webviews/knowledgeGraph/knowledgeGraph.ts
@@ -1,14 +1,15 @@
 import * as vscode from 'vscode';
 import * as mockKnowledgeGraph from '../../api/mockKnowledgeGraph';
 
-export function knowledgeGraphHTML(cssUri:vscode.Uri, scriptUri: vscode.Uri) {
-    //let mockData = mockKnowledgeGraph.
+export function knowledgeGraphHTML(cssUri:vscode.Uri, scriptUri: vscode.Uri, d3Uri: vscode.Uri) {
+    let mockData = mockKnowledgeGraph.mockKnowledgeGraphGETRequest(46);
     return `
     <!DOCTYPE HTML>
     <HTML>
         <head>
             <meta charset="UTF-8" lang="en"/>
             <link rel="stylesheet" type="text/css" href="${cssUri}"/>
+            <script src="${d3Uri}"></script>
         </head>
         <body>
             <div class="page">
