3813c7e7e45bb6349da36bdc3753ece40ed0b5b9
isaiahsherfick <isaiahsherfick8@gmail.com>
2022-04-06 09:49:45 -0400

    Added utils for running git status

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,7 +1,4 @@
 import * as assert from "assert";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from "vscode";
 import * as mockCodeMap from "../../api/mockCodeMap";
 import {
@@ -9,7 +6,6 @@ import {
   mockCoauthorshipNetworkGETRequest,
 } from "../../api/mockCoauthorshipNetwork";
 import * as config from "../../config/config";
-// import * as myExtension from '../../extension';
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
