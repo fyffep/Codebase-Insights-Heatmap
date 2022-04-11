d5685da74fc55269a79e323c1e9ab69ea6d55549
isaiahsherfick <isaiahsherfick8@gmail.com>
2022-04-11 13:40:45 -0400

    Removed login and risk assessment tabs for release

--- src/treeviews/codeBaseInsightsViewProvider.ts
+++ src/treeviews/codeBaseInsightsViewProvider.ts
@@ -41,7 +41,7 @@ export class CodebaseInsightsViewProvider
       vscode.TreeItemCollapsibleState.None,
       "View your changes currently staged for commit and assess the potential risks associated with your changes"
     );
-    views = [loginSignup, settings, map, coauthorship, risk];
+    views = [settings, map, coauthorship];
     return views;
   }
 }
